10718b74b2e6ecc864a4ea8734eb18b41357e7d2
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-01-13 21:02:03 -0300

    introducao ao fecth

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('sucesso')
+//requisição http
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url).then(function(response){
+    console.log(response)
+})
+
+console.log('teste')
+
